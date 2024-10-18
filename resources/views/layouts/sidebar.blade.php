6187ab1d82004c29a047e541943d4a6837659396
Ni Nyoman Amerta Kusuma Yanti <kusumayanti114@gmail.com>
2024-10-19 01:55:49 +0700

    add layout

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,51 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item" style="padding-top: 10px">
+            <a class="nav-link collapsed " href="#">
+                <i class="bi bi-house-door"></i>
+                <span>Home</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-search"></i>
+                <span>Cari</span>
+            </a>
+        </li><!-- End Register Page Nav -->
+
+
+
+
+        <!-- <li class="nav-item">
+        <a class="nav-link collapsed" href="users-profile.php">
+          <i class="bi bi-person"></i>
+          <span>Profile</span>
+        </a>
+      </li>End Profile Page Nav -->
+
+        <!-- <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-login.php">
+          <i class="bi bi-box-arrow-in-right"></i>
+          <span>Login</span>
+        </a>
+      </li>End Login Page Nav
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-register.php">
+          <i class="bi bi-card-list"></i>
+          <span>Register</span>
+        </a>
+      </li>End Register Page Nav -->
+
+
+
+
+
+
+    </ul>
+
+</aside><!-- End Sidebar-->
