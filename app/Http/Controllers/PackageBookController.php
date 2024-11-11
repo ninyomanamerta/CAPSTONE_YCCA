b531cce7a44201dfe42557edca776284393001fd
Ni Nyoman Amerta Kusuma Yanti <kusumayanti114@gmail.com>
2024-11-11 16:48:11 +0700

    add model buku paket

---
+++ app/Http/Controllers/PackageBookController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PackageBook;
+use Illuminate\Http\Request;
+
+class PackageBookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PackageBook $packageBook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PackageBook $packageBook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PackageBook $packageBook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PackageBook $packageBook)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_11_11_151701_create_table_detail_package_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_package_books', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('id_package_books');
+            $table->integer('nomor_induk');
+
+            $table->foreign('id_package_books')->references('id')->on('package_books')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_detail_package_books');
+    }
+};
