afd19b07b96ec6980ef7b930118400ebb440abe5
Ni Nyoman Amerta Kusuma Yanti <kusumayanti114@gmail.com>
2024-11-22 21:30:47 +0700

    notification

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -25,13 +25,13 @@
 
           <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
             <i class="bi bi-bell"></i>
-            <span class="badge bg-danger badge-number">4</span>
+            <span class="badge bg-danger badge-number">{{ $terlambatCount }}</span>
           </a><!-- End Notification Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
             <li class="dropdown-header">
-              You have 4 new notifications
-              <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+              Terlambat Mengembalikan Buku!!!
+              <a href="{{ route('peminjamanbukupengayaan.index') }}"><span class="badge rounded-pill bg-primary p-2 ms-2">Lihat Semua</span></a>
             </li>
             <li>
               <hr class="dropdown-divider">
@@ -40,13 +40,13 @@
             <li class="notification-item">
               <i class="bi bi-exclamation-circle text-warning"></i>
               <div>
-                <h4>Lorem Ipsum</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>30 min. ago</p>
+                <h4>Terlambat Mengembalikan Buku</h4>
+                <p>Jumlah Buku Yang Belum Dikembalian</p>
+                <p>{{ $terlambatCount }}</p>
               </div>
             </li>
 
-            <li>
+            {{-- <li>
               <hr class="dropdown-divider">
             </li>
 
@@ -90,7 +90,7 @@
             </li>
             <li class="dropdown-footer">
               <a href="#">Show all notifications</a>
-            </li>
+            </li> --}}
 
           </ul><!-- End Notification Dropdown Items -->
 
